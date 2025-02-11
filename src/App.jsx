8cc42fc581cd14b785fd2580c2bd99d165b14de4
wilfredtinega <lowkieman@gmail.com>
2025-02-11 09:48:15 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import react from "./assets/react.svg"
+
+function App() {
+  return (
+    <div className='flex items-center justify-center bg-slate-500 min-h-[100vh] text-gray-200'>
+      Real Estate
+      <img className='p-1' src={react} alt="" />
+
+    </div>
+  )
+}
+
+export default App
